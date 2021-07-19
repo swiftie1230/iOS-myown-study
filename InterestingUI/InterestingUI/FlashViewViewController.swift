a52c6b215d14b5982180b19b075fea7728ab6332
서진 <63195670+swiftie1230@users.noreply.github.com>
2021-07-19 17:57:37 +0900

    Add files via upload

---
+++ InterestingUI/InterestingUI/FlashViewViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FlashViewViewController.swift
+//  InterestingUI
+//
+//  Created by 황서진 on 2021/07/19.
+//
+
+import UIKit
+
+class FlashViewViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+    @IBOutlet var Image: UIImageView!
+    @IBOutlet var titleKor: UILabel!
+    @IBOutlet var titleEng: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onButtonClicked(_ sender: Any) {
+        view.backgroundColor = UIColor(red: 225 / 255.0, green: 252 / 255.0, blue: 187 / 255.0, alpha: 1)
+        label.textColor = UIColor.black
+        titleKor.textColor = UIColor.black
+        titleKor.text = "손전등을 끄고 싶으신가요?"
+        titleEng.text = "Wanna turn off your flashlight?"
+        Image.image = UIImage(systemName: "flashlight.on.fill")
+    }
+    @IBAction func offButtonClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+        label.textColor = UIColor.white
+        titleKor.textColor = UIColor.white
+        titleKor.text = "손전등을 키고 싶으신가요?"
+        titleEng.text = "Wanna light your flashlight?"
+        Image.image = UIImage(systemName: "flashlight.off.fill")
+    }
+}
---
+++ InterestingUI/InterestingUI/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  InterestingUI
+//
+//  Created by 황서진 on 2021/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    //@IBAction func buttonClicked(_ sender: UIButton) {
+        // 소로로록 애니메이션
+//        UIView.animate(withDuration: 0.6) {
+//            self.imageView.frame = CGRect(x: 120, y: 120, width: 200, height: 200)
+//        }
+        
+        
+//        // view1 생성
+//        var view1 = UIView(frame: CGRect(x: 20, y: 100, width: 200, height: 200))
+//        // view1의 color black 설정
+//        view1.backgroundColor = UIColor.black
+//        // view에 view1 add
+//        self.view.addSubview(view1)
+//
+//        var view2 = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//        view2.backgroundColor = UIColor.red
+//        view1.addSubview(view2) // view2의 상위 뷰는 view1이다.
+       
+    
+    //}
+}
+

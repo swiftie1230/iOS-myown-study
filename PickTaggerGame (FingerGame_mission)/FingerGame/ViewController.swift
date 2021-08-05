a86ac92da2aec0ce32aea2e860de869f94df3798
서진 <63195670+swiftie1230@users.noreply.github.com>
2021-08-05 22:30:39 +0900

    Add files via upload

---
+++ PickTaggerGame (FingerGame_mission)/FingerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FingerGame
+//
+//  Created by 황서진 on 2021/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
